d84603871b53ce2983c7578a9b20e66ae6342d5a
avidrutham <divyamathur89@gmail.com>
2020-08-07 21:16:34 +0530

    delete user subscription to auth state changes from application and create user provider for Authentication component

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import "./index.scss";
 
 import Application from "./components/Application";
 import PostsProvider from "./provider/PostsProvider";
+import UserProvider from "./provider/UserProvider";
 
 render(
-  <PostsProvider>
-    <Application />
-  </PostsProvider>,
+  <UserProvider>
+    <PostsProvider>
+      <Application />
+    </PostsProvider>
+  </UserProvider>,
   document.getElementById("root")
 );
